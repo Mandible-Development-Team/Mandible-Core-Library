626af15151d17451a9d7f1ae1ae08c124c0c90d3
Aja-Espana <aja_espana@outlook.com>
2026-02-08 13:57:57 -0500

    Added runtime object class

---
+++ Runtime/Systems/Data/RuntimeObject/RuntimeObject.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEditor;
+
+namespace Mandible.Systems.Data
+{
+    public abstract class RuntimeObject : ScriptableObject
+    {
+        protected bool isInstance = false;
+
+        // Getters
+        public bool IsInstance => isInstance;
+
+        /// <summary>
+        /// Returns a runtime clone of the actual derived SO type
+        /// </summary>
+
+        public RuntimeObject CreateRuntimeInstance()
+        {
+            RuntimeObject instance = Instantiate(this);
+            instance.OnInstantiate();
+
+            return instance;
+        }
+
+        private void OnInstantiate()
+        {
+            isInstance = true;
+        }
+    }
+}
